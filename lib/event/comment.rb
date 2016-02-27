4da912329e49944c9db379953dfa83c1823891cb
Jens Balvig <jens@balvig.com>
2016-02-27 21:51:03 +0100

    refactor

--- lib/event/comment.rb
+++ lib/event/comment.rb
@@ -4,17 +4,24 @@ class Event
     REVIEWED_COLOR = "207de5"
 
     def process
-      init_labels
-      if body.include?(":+1:")
-        github.add_labels_to_an_issue(repo, number, [REVIEWED_LABEL])
-      elsif body.include?(":recycle:")
-        github.remove_label(repo, number, REVIEWED_LABEL)
+      case
+      when body.include?(":+1:")      then add_reviewed_label
+      when body.include?(":recycle:") then remove_reviewed_label
       end
     end
 
     private
 
-      def init_labels
+      def add_reviewed_label
+        setup_label
+        github.add_labels_to_an_issue(repo, number, [REVIEWED_LABEL])
+      end
+
+      def remove_reviewed_label
+        github.remove_label(repo, number, REVIEWED_LABEL)
+      end
+
+      def setup_label
         github.label(repo, REVIEWED_LABEL)
       rescue Octokit::NotFound
         github.add_label(repo, REVIEWED_LABEL, REVIEWED_COLOR)
