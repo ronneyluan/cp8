6ee4587c9757fec3ee59430cfd18f0390a538a57
Jens Balvig <jens@balvig.com>
2016-02-27 21:46:30 +0100

    test branches

--- test/payload_test.rb
+++ test/payload_test.rb
@@ -5,20 +5,29 @@ class PayloadTest < Minitest::Test
     Cp8.github_client = github
   end
 
-  def test_it_does_something_useful
+  def test_creating_labels_if_they_dont_existdoes_something_useful
     github.expects(:label).with("balvig/bornholm", "Reviewed").once.raises(Octokit::NotFound)
     github.expects(:add_label).with("balvig/bornholm", "Reviewed", "207de5")
+    Payload.new_from_json(json(:comment_plus_one)).process
+  end
+
+  def test_adding_reviewed_label_if_given_plus_one
     github.expects(:add_labels_to_an_issue).with("balvig/bornholm", 1, ["Reviewed"]).once
-    Payload.new_from_json(json).process
+    Payload.new_from_json(json(:comment_plus_one)).process
+  end
+
+  def test_adding_reviewed_label_if_given_recyle
+    github.expects(:remove_label).with("balvig/bornholm", 1, "Reviewed").once
+    Payload.new_from_json(json(:comment_recycle)).process
   end
 
   private
 
-    def json
-      @json ||= File.read File.expand_path("../fixtures/comment.json", __FILE__)
+    def json(file)
+      @json ||= File.read File.expand_path("../fixtures/#{file}.json", __FILE__)
     end
 
     def github
-      @github ||= mock
+      @github ||= stub(label: true, add_label: true, add_labels_to_an_issue: true)
     end
 end
