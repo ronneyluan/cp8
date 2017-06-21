83fb6e6304df85a0c980bc93a6014d476fbac786
Jens Balvig <jens@balvig.com>
2017-06-21 12:17:05 +0900

    show better output for errors

--- app.rb
+++ app.rb
@@ -7,7 +7,14 @@ Bundler.require :default, ENV["RACK_ENV"].to_sym
 
 require "cp8"
 
+set :show_exceptions, false
+
 post "/payload" do
   Payload.new_from_json(request.body.read).process
   "Done!"
+end
+
+error 500 do
+  error = env["sinatra.error"]
+  "#{error.class}: #{error.message}"
 end
