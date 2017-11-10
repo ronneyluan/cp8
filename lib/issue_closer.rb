6be573b84e1aa42e2e343f38ed94529cdea2a2de
Jens Balvig <jens@balvig.com>
2017-11-10 15:31:33 +0900

    wip

---
+++ lib/issue_closer.rb
@@ -0,0 +1,31 @@
+class IssueCloser
+  WEEK = 7 * 24 * 60 * 60
+
+  def initialize(repo, weeks: 4)
+    @repo = repo
+    @weeks = weeks
+  end
+
+  def run
+    stale_issues.each do |issue|
+      github.close_issue(repo, issue.number)
+      github.add_comment(repo, issue.number, "[BEEP BOOP] Hi there!\n\nThis issue/PR hasn't been updated in _a month_ so am closing it for now.\n\nFeel free to re-open in the future if/when it becomes relevant again! :heart:")
+    end
+  end
+
+  private
+
+    attr_reader :repo, :weeks
+
+    def stale_issues
+      github.search_issues("repo:#{repo} is:open updated:<#{stale_pr_cutoff_date}").items
+    end
+
+    def stale_pr_cutoff_date
+      (Time.now - weeks * WEEK).iso8601
+    end
+
+    def github
+      Cp8.github_client
+    end
+end
--- lib/processor.rb
+++ lib/processor.rb
@@ -1,5 +1,4 @@
 class Processor
-  WEEK = 7 * 24 * 60 * 60
 
   def initialize(payload)
     @payload = payload
@@ -20,10 +19,7 @@ class Processor
     def close_stale_issues
       return if event_triggered_by_cp8?
 
-      stale_issues.each do |issue|
-        github.close_issue(repo, issue.number)
-        github.add_comment(repo, issue.number, "[BEEP BOOP] Hi there!\n\nThis issue/PR hasn't been updated in _a month_ so am closing it for now.\n\nFeel free to re-open in the future if/when it becomes relevant again! :heart:")
-      end
+      IssueCloser.new(repo).run
     end
 
     def event_triggered_by_cp8?
@@ -36,14 +32,6 @@ class Processor
 
     def repo
       payload.repo
-    end
-
-    def stale_issues
-      github.search_issues("repo:#{repo} is:open updated:<#{stale_pr_cutoff_date}").items
-    end
-
-    def stale_pr_cutoff_date
-      (Time.now - 4 * WEEK).iso8601
     end
 
     def github
